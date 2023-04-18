d9e5f6f01212aa2b758ffbbd5b5254e89e485629
fileg <fileg@DESKTOP-M2ECFMB>
2023-04-19 01:13:21 +0300

    Add project files

--- ContactManagment/ContactManagment.cpp
+++ ContactManagment/ContactManagment.cpp
@@ -4,17 +4,11 @@ void save(list <Contact*> );
 void display(list <Contact*>);
 void createlist(list <Contact*> *);
 void deleteCon(list <Contact*>*);
+void createAcc(list <Contact*>*);
 int main()
 {
     list <Contact* > a;
-    int flag = 0 ,flag3=0;
-    char* name = new char[50];
-    char* name2 = new char[50];
-    char* surname = new char[50];
-    char* phone = new char[10];
-    char* name3 = new char[50];
-    char* surname4 = new char[50];
-    char* surname2 = new char[50];
+    int flag = 0;
     cout << "Contact Managment!" << endl;
     createlist(&a);
     while (flag != 4)
@@ -24,54 +18,7 @@ int main()
         cin >> flag;
         if (flag == 1)
         {
-            cout << "Give a name : " << endl;
-            cin >> name;
-            cout << "Give a surname : " << endl;
-            cin >> surname;
-            cout << "Give a phone : " << endl;
-            cin >> phone;
-            cout << "Do you want to add more informations ? [Y/N]" << endl;
-            char epil;
-            cin >> epil;
-            ifstream fp2;
-            fp2.open("contacts.txt");
-            while (!fp2.eof())
-            {
-                fp2.getline(name3, 50);
-                fp2.getline(surname4, 50);
-                if (strcmp(name, name3) == 0 && strcmp(surname,surname4)==0)
-                {
-                    cout << "Same" << endl;
-                    flag3 = 1;
-                    break;
-                }
-            }
-            if (flag3 == 0)
-            {
-                if (epil == 'Y' || epil == 'y')
-                {
-                    string company;
-                    cout << "Give a company name : " << endl;
-                    cin >> company;
-                    string email;
-                    cout << "Give a email : " << endl;
-                    cin >> email;
-                    Contact* contact1 = new Contact(name, surname, company, phone, email);
-                    a.push_back(contact1);      
-                }
-                else
-                {
-                    Contact* contact2 = new Contact(name, surname, phone);
-                    a.push_back(contact2);
-                }
-                cout << "You create a contact " << endl;
-                save(a);
-            }
-            else
-            {
-                cout << "The contact has already exists" << endl;
-            }
-            fp2.close();
+            createAcc(&a);
         }
         else if (flag == 3)
         {
@@ -89,13 +36,6 @@ int main()
 
     }
     cout << "Bye" << endl;
-    delete[] name;
-    delete[] surname;
-    delete[] name2;
-    delete[] surname2;
-    delete[] name3;
-    delete[] surname4;
-    delete[] phone;
     a.clear();
     return 0;
 }
@@ -181,4 +121,69 @@ void deleteCon(list <Contact*> *a )
     createlist(a);
     if (f == 0)
         cout << "You can not delete this contact because it not exists" << endl;
+    delete[]name2;
+    delete[]surname2;
+}
+void createAcc(list <Contact*> *a)
+{
+    int flag3 = 0;
+    char* name = new char[50];
+    char* surname = new char[50];
+    char* phone = new char[10];
+    char* name3 = new char[50];
+    char* surname4 = new char[50];
+    Contact* contact1 , *contact2;
+    cout << "Give a name : " << endl;
+    cin >> name;
+    cout << "Give a surname : " << endl;
+    cin >> surname;
+    cout << "Give a phone : " << endl;
+    cin >> phone;
+    cout << "Do you want to add more informations ? [Y/N]" << endl;
+    char epil;
+    cin >> epil;
+    ifstream fp2;
+    fp2.open("contacts.txt");
+    while (!fp2.eof())
+    {
+        fp2.getline(name3, 50);
+        fp2.getline(surname4, 50);
+        if (strcmp(name, name3) == 0 && strcmp(surname, surname4) == 0)
+        {
+            cout << "Same" << endl;
+            flag3 = 1;
+            break;
+        }
+    }
+    if (flag3 == 0)
+    {
+        if (epil == 'Y' || epil == 'y')
+        {
+            string company;
+            cout << "Give a company name : " << endl;
+            cin >> company;
+            string email;
+            cout << "Give a email : " << endl;
+            cin >> email;
+            contact1 = new Contact(name, surname, company, phone, email);
+            (*a).push_back(contact1);
+        }
+        else
+        {
+            contact2 = new Contact(name, surname, phone);
+            (*a).push_back(contact2);
+        }
+        cout << "You create a contact " << endl;
+        save(*a);
+    }
+    else
+    {
+        cout << "The contact has already exists" << endl;
+    }
+    fp2.close();
+    delete[] name;
+    delete[] surname;
+    delete[] name3;
+    delete[] surname4;
+    delete[] phone;
 }
